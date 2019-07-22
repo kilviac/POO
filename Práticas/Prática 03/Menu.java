546c0bf7b72b3bc549cf25a5afd724170d8b4957
Kilvia Carvalho <carvalho.kilvia@gmail.com>
2019-07-22 16:36:55 -0300

    Classes e Objetos em Java

---
+++ Práticas/Prática 03/Menu.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Menu {
+    private ContaCorrente conta;
+    double quantia;
+
+    public Menu(ContaCorrente conta) {
+        this.conta = conta;
+    }
+
+    public void exibirMenu(){
+        Scanner read = new Scanner(System.in);
+        int opcao = 0;
+        System.out.println("--- Projeto Conta Corrente ---\n1. Saque\n2. Depósito\n3. Saldo\n4. Extrato\n5. SAIR\n------------------------------");
+
+        while(opcao != 5){
+            System.out.print("\nDigite uma opção: ");
+            opcao = read.nextInt();
+
+            if (opcao == 1) {
+                System.out.print("Entre com a quantia a ser depositada: ");
+                quantia = read.nextInt();
+                if (quantia > 0) {
+                    conta.deposito(quantia);
+                    System.out.println("Depósito feito com sucesso!");
+                } else {
+                    System.out.println("Quantia inválida!");
+                }
+            }
+
+            if (opcao == 2) {
+                System.out.print("Entre com a quantia a ser sacada: ");
+                quantia = read.nextInt();
+                if (quantia > 0 && quantia <= conta.getSaldo()) {
+                    conta.saque(quantia);
+                    System.out.println("Saque feito com sucesso!");
+                } else {
+                    System.out.println("Quantia inválida!");
+                }
+            }
+
+            if (opcao == 3) {
+                System.out.println("Saldo: R$" + conta.getSaldo());
+            }
+
+            if (opcao == 4) {
+                System.out.println("\n--- Extrato ---");
+                conta.extrato();
+            }
+
+            if (opcao == 5) {
+                System.out.println("Encerrando...");
+            }
+        }
+    }
+}
